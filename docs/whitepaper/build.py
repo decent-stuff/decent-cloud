55985bf539e3736a76925bfb8378658282a92adb
Yan Liu <yanliu8@proton.me>
2024-08-25 17:16:12 +0200

    docs: add support for --watch in the build script

--- docs/whitepaper/build.py
+++ docs/whitepaper/build.py
@@ -43,7 +43,7 @@ def run_mmdc(repo_root: pathlib.Path, input_path: pathlib.Path, output_path: pat
         print("Error: Mermaid conversion failed.")
         sys.exit(1)
 
-def run_latexmk(repo_root: pathlib.Path, input_path: pathlib.Path, cache_dir: pathlib.Path):
+def run_latexmk(repo_root: pathlib.Path, input_path: pathlib.Path, cache_dir: pathlib.Path, watch: bool):
     """Build the pdf using latexmk docker container."""
     print(f"Building PDF: {input_path.name}")
     cache_subdir = input_path.parent.relative_to(repo_root)
@@ -57,6 +57,10 @@ def run_latexmk(repo_root: pathlib.Path, input_path: pathlib.Path, cache_dir: pa
         '-view=none', str(input_path.name),
         f'-output-directory=/cache/{cache_subdir}'
     ]
+
+    if watch:
+        args.append('-pvc')
+
     print('Running:', ' '.join(args))
     subprocess.check_call(args)
     output_pdf = cache_dir / cache_subdir / "whitepaper.pdf"
@@ -64,13 +68,6 @@ def run_latexmk(repo_root: pathlib.Path, input_path: pathlib.Path, cache_dir: pa
         print("Error: Latexmk conversion failed.")
         sys.exit(1)
 
-def build_latex(repo_root: pathlib.Path, latex_dir: pathlib.Path, cache_dir: pathlib.Path):
-    """Build the pdf using latexmk docker container."""
-    latex_files = list(latex_dir.glob('*.tex')) + list(latex_dir.glob('*.cls')) + list(latex_dir.glob('*.bib')) + list(latex_dir.glob('*.bbl')) + list(latex_dir.glob('*.600pk'))
-    pdf_file = cache_dir / latex_dir.relative_to(repo_root) / "whitepaper.pdf"
-
-    if should_rebuild(latex_files, pdf_file):
-        run_latexmk(repo_root=repo_root, input_path=latex_dir / 'whitepaper.tex', cache_dir=cache_dir)
 
 def build_figures(repo_root: pathlib.Path, figures_dir: pathlib.Path):
     for mmd_file in figures_dir.glob('*.mmd'):
@@ -79,12 +76,13 @@ def build_figures(repo_root: pathlib.Path, figures_dir: pathlib.Path):
             run_mmdc(repo_root=repo_root, input_path=mmd_file, output_path=output_path)
 
 
-def build_latex(repo_root: pathlib.Path, latex_dir: pathlib.Path, cache_dir: pathlib.Path):
+def build_latex(repo_root: pathlib.Path, latex_dir: pathlib.Path, cache_dir: pathlib.Path, watch: bool):
+    """Build the pdf using latexmk docker container."""
     latex_files = list(latex_dir.glob('*.tex')) + list(latex_dir.glob('*.cls')) + list(latex_dir.glob('*.bib')) + list(latex_dir.glob('*.bbl')) + list(latex_dir.glob('*.600pk'))
     pdf_file = cache_dir / latex_dir.relative_to(repo_root) / "whitepaper.pdf"
 
-    if should_rebuild(latex_files, pdf_file):
-        run_latexmk(repo_root=repo_root, input_path=latex_dir / 'whitepaper.tex', cache_dir=cache_dir)
+    if should_rebuild(latex_files, pdf_file) or watch:
+        run_latexmk(repo_root=repo_root, input_path=latex_dir / 'whitepaper.tex', cache_dir=cache_dir, watch=watch)
 
 def get_repo_root():
     try:
@@ -101,6 +99,7 @@ def clean(cache_dir: pathlib.Path):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--clean', action='store_true')
+    parser.add_argument('--watch', action='store_true')
     return parser.parse_args()
 
 def main():
@@ -116,15 +115,7 @@ def main():
     cache_dir.mkdir(parents=True, exist_ok=True)
 
     build_figures(repo_root, repo_root / "docs" / "whitepaper" / "figures")
-    build_latex(repo_root, repo_root / "docs" / "whitepaper", cache_dir)
-
-    output_pdf = cache_dir / "docs" / "whitepaper" / "whitepaper.pdf"
-    final_output_path = repo_root / "docs" / "whitepaper" / "whitepaper.pdf"
-    if output_pdf.exists():
-        shutil.move(str(output_pdf), str(final_output_path))
-        print(f"PDF written to {final_output_path}")
-    else:
-        print("Error: PDF was not generated.")
+    build_latex(repo_root, repo_root / "docs" / "whitepaper", cache_dir, args.watch)
 
 if __name__ == '__main__':
     main()
