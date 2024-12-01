a09b2ce2ae38fe0613f1453bd29b884a2a3d48e1
Yan Liu <yanliu8@proton.me>
2024-12-01 18:56:47 +0100

    Build canister only on x86_64 linux

--- ic-canister/build.rs
+++ ic-canister/build.rs
@@ -15,38 +15,45 @@ fn workspace_dir() -> PathBuf {
 }
 
 pub fn main() {
-    if Ok("x86_64") == env::var("CARGO_CFG_TARGET_ARCH").as_deref() {
-        let workspace_dir_path = workspace_dir();
-        let canister_dir_path = workspace_dir_path.join("ic-canister");
-        println!("Building canister... {}", canister_dir_path.display());
-
-        // List of environment variables that may influence the build process
-        let vars_to_unset = [
-            "RUSTFLAGS",
-            "CARGO_ENCODED_RUSTFLAGS",
-            "CC",
-            "CXX",
-            "LD",
-            "AR",
-            "CARGO_TARGET_DIR",
-            "CARGO_BUILD_TARGET",
-        ];
-
-        // Iterate over the list and remove each variable
-        for var in vars_to_unset.iter() {
-            std::env::remove_var(var);
-        }
-
-        if !Command::new("dfx")
-            .args(["build"])
-            .current_dir(canister_dir_path)
-            .status()
-            .expect("Failed to build canister")
-            .success()
-        {
-            panic!("failed to build canister")
-        };
+    // Only build the canister on x86_64 Linux
+    if Ok("linux") != env::var("CARGO_CFG_TARGET_OS").as_deref() {
+        return;
     }
+
+    if Ok("x86_64") != env::var("CARGO_CFG_TARGET_ARCH").as_deref() {
+        return;
+    }
+
+    let workspace_dir_path = workspace_dir();
+    let canister_dir_path = workspace_dir_path.join("ic-canister");
+    println!("Building canister... {}", canister_dir_path.display());
+
+    // List of environment variables that may influence the build process
+    let vars_to_unset = [
+        "RUSTFLAGS",
+        "CARGO_ENCODED_RUSTFLAGS",
+        "CC",
+        "CXX",
+        "LD",
+        "AR",
+        "CARGO_TARGET_DIR",
+        "CARGO_BUILD_TARGET",
+    ];
+
+    // Iterate over the list and remove each variable
+    for var in vars_to_unset.iter() {
+        std::env::remove_var(var);
+    }
+
+    if !Command::new("dfx")
+        .args(["build"])
+        .current_dir(canister_dir_path)
+        .status()
+        .expect("Failed to build canister")
+        .success()
+    {
+        panic!("failed to build canister")
+    };
 
     let result = which::which("pocket-ic").expect(
         "Failed to find pocket-ic server binary. Please run `pixi run install-pocket-ic-server`",
