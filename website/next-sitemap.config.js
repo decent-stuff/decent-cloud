3487afb1d5b57d7da98c9f147af6bb2094f10dcc
Yan Liu <yanliu8@proton.me>
2025-11-08 19:14:42 +0100

    Revert "add sitemap"
    This reverts commit 729817bc5bae544cb42017df61570a4dfc215fd7.

--- website/next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://decent-cloud.org',
-    generateRobotsTxt: true,
-    outDir: './out',
-}
