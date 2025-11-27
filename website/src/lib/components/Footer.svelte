2bcfb6217618e14dcda40b2ca912d90b7d2f87fd
Yan Liu <yanliu8@proton.me>
2025-11-27 22:38:08 +0100

    docs: update github link in footer
    - Updated the github link

--- website/src/lib/components/Footer.svelte
+++ website/src/lib/components/Footer.svelte
@@ -3,29 +3,58 @@
 		<div class="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
 			<div>
 				<h3 class="text-xl font-bold mb-4">Decent Cloud</h3>
-				<p class="text-white/60">Decentralized cloud computing for everyone</p>
+				<p class="text-white/60">
+					Decentralized cloud computing for everyone
+				</p>
 			</div>
 			<div>
 				<h4 class="font-semibold mb-4">Product</h4>
 				<ul class="space-y-2 text-white/60">
-					<li><a href="/dashboard/marketplace" class="hover:text-white">Marketplace</a></li>
-					<li><a href="/dashboard/offerings" class="hover:text-white">Offerings</a></li>
+					<li>
+						<a
+							href="/dashboard/marketplace"
+							class="hover:text-white">Marketplace</a
+						>
+					</li>
+					<li>
+						<a href="/dashboard/offerings" class="hover:text-white"
+							>Offerings</a
+						>
+					</li>
 				</ul>
 			</div>
 			<div>
 				<h4 class="font-semibold mb-4">Resources</h4>
 				<ul class="space-y-2 text-white/60">
-					<li><a href="/docs/decent-cloud-whitepaper.pdf" class="hover:text-white">Whitepaper</a></li>
-					<li><a href="https://github.com/decent-cloud" class="hover:text-white">GitHub</a></li>
-					<li><a href="/ledger" class="hover:text-white">Ledger</a></li>
+					<li>
+						<a
+							href="/docs/decent-cloud-whitepaper.pdf"
+							class="hover:text-white">Whitepaper</a
+						>
+					</li>
+					<li>
+						<a
+							href="https://github.com/decent-stuff/decent-cloud"
+							class="hover:text-white">GitHub</a
+						>
+					</li>
 				</ul>
 			</div>
 			<div>
 				<h4 class="font-semibold mb-4">Community</h4>
 				<ul class="space-y-2 text-white/60">
-					<li><a href="https://discord.gg/decentcloud" class="hover:text-white">Discord</a></li>
-					<li><a href="https://twitter.com/decentcloud" class="hover:text-white">Twitter</a></li>
-					<li><a href="https://forum.decentcloud.io" class="hover:text-white">Forum</a></li>
+					<li>
+						<a
+							href="https://discord.gg/decentcloud"
+							class="hover:text-white">Discord</a
+						>
+					</li>
+					<li>
+						<a
+							href="https://twitter.com/decentcloud"
+							class="hover:text-white">Twitter</a
+						>
+					</li>
 				</ul>
 			</div>
 		</div>
